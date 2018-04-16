2adb176ca549b97573f093ad216b43ca88d2ed30
Keith Witelson <witelsonkeith@gmail.com>
2018-04-16 14:46:50 +0200

    Enemy now goes to waypoints in whicever order they're present in the inspector.

--- NavMeshTD/Assets/Scripts/EnemyMove.cs
+++ NavMeshTD/Assets/Scripts/EnemyMove.cs
@@ -6,13 +6,26 @@ using UnityEngine.AI;
 public class EnemyMove : MonoBehaviour {
 
 	public NavMeshAgent agent;
+
+	private int waypointIndex = 0;
+	private Vector3 currentTarget;
 	// Use this for initialization
 	void Start () {
-		agent.SetDestination (new Vector3 (-10f, 1.5f, 10f));
+		currentTarget = Waypoints.points [0].position;
+		currentTarget.y = 1f;
+		agent.SetDestination (currentTarget);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (waypointIndex < Waypoints.points.Length-1) {
+			if (Vector3.Distance (this.transform.position, currentTarget) <= 0.45f) {
+				waypointIndex += 1;
+				currentTarget = Waypoints.points [waypointIndex].position;
+				currentTarget.y = 1f;
+				print (this.transform.position - currentTarget);
+				agent.SetDestination (currentTarget);
+			}
+		}
 	}
 }
